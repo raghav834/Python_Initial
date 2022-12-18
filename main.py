1e48abcd1ef0a254e642e209ae6d40d7760e47e7
raghav834 <haktgam@gmail.com>
2022-12-18 06:38:37 +0000

    Identify City are in same country

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-n=int(input('Enter a Number : '))
+#ex 1 part b
+india=['mumbai','banglore','chennai','delhi']
+pakistan=['lahore','karachi','islamabad']
+bangladesh=['dhaka','khulna','rangpur']
 
-if n%2==0:
-    print(f'{n} is even')
+city1=input('Enter Name of city1 : ')
+city2=input('Enter name of city2 : ')
+
+
+if city1 in india and city2 in india:
+    print(f'{city1} and {city2} are in india' )
+elif city1 in india and city2 in pakistan:
+    print(f'{city1} and {city2} are in pakistan' )
+elif city1 in india and city2 in bangladesh:
+    print(f'{city1} and {city2} are in bangladesh')
 else:
-    print(f'{n} is odd')
+    print(f'{city1} and {city2} are either not in same country or one or both of them are not in any country')
