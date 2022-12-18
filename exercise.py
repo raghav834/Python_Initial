a49af19b76a4ee50538703c62e4ebaa758910434
raghav834 <haktgam@gmail.com>
2022-12-18 13:33:28 +0000

    Type expense get month(using if else)

--- exercise.py
+++ exercise.py
@@ -1,6 +1,11 @@
-#2
-for i in range(1,11):
-    if i%2==0:
-        continue
-    else:
-        print(i**2)
+#3
+expense_list = [2340, 2500, 2100, 3100, 2980]
+month=['january','febraury','march','april','may']
+
+expense = int(input("Enter the expense in rupees : "))
+
+if  expense in expense_list:
+   index= expense_list.index(expense)
+   print('This is the expense of '+month[index])
+else:
+    print(f"{expense} is not the expense of any month")
